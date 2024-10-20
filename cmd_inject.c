279a04dbf9987d68f6c672f11d7ccd81c53b7799
tjandra <tjandra.satria@sci.ui.ac.id>
2024-10-21 03:01:44 +0700

    v0.2.2: remove the limit on max number of distinct arguments that the injector can receive to basicly unlimited (still limited by 32 bit signed integer, but who wrote that much arguments to a program)

--- cmd_inject.c
+++ cmd_inject.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define CMD_INJECT_VERSION "v0.2.1"
+#define CMD_INJECT_VERSION "v0.2.2"
 int is_both_string_equal(char*a,char*b)
 {
 	for(;*a!='\0'&&*b!='\0';)if(*a++!=*b++)return 0;
-	return *a==*b;
+	return*a==*b;
 }
-typedef struct Dynamic_String{char *data;int length,alloc;}dstr;
-int check_if_str_is_prefix_of_dstr(dstr *str,int str_index,char *keyword)
+typedef struct Dynamic_String{char*data;int length,alloc;}dstr;
+int check_if_str_is_prefix_of_dstr(dstr*str,int str_index,char*keyword)
 {
-	while(*keyword!='\0')
-		if(str_index>=str->length||str->data[str_index++]!=*keyword++)return 0;
+	while(*keyword!='\0'&&str_index<str->length)
+		if(str->data[str_index++]!=*keyword++)return 0;
 	return*keyword=='\0';
 }
 int check_for_special_program_keyword(dstr*str,int left_index,int right_index)
 {
-	//convert the format
+	//generating data backup before doing check operation
 	char backup_char,left_backup,right_backup;
 	if(str->data[left_index]==str->data[right_index]&&
 		(str->data[left_index]=='\''||str->data[left_index]=='\"'))
@@ -32,10 +32,13 @@ int check_for_special_program_keyword(dstr*str,int left_index,int right_index)
 		left_backup=str->data[--left_index];
 		right_backup=str->data[++right_index];
 	}
+	//change the data format to make checking process easier
 	str->data[left_index]=str->data[right_index]='\"';
 	backup_char=str->data[right_index+1];
 	str->data[right_index+1]='\0';
 	str->data+=left_index;
+	str->length-=left_index;
+	//begin checking
 	int satisfy=0,i,ret_code=0;
 	if(is_both_string_equal(str->data,"\"waitforexitandrun\""))
 	{
@@ -44,7 +47,7 @@ int check_for_special_program_keyword(dstr*str,int left_index,int right_index)
 	}
 	for(i=right_index-left_index+1;i--;)if(str->data[i]=='\\'||str->data[i]=='/')break;
 	if(i<0)i=0;
-	if(check_if_str_is_prefix_of_dstr(str,++i,"proton")){i+=6;satisfy=1;}//proton_binary
+	if(check_if_str_is_prefix_of_dstr(str,++i,"proton")){i+=6;satisfy=1;}//proton binary
 	else if(check_if_str_is_prefix_of_dstr(str,i,"wine")){i+=4;satisfy=1;}//wine binary
 	if(!satisfy)
 	{
@@ -57,7 +60,7 @@ int check_for_special_program_keyword(dstr*str,int left_index,int right_index)
 			check_if_str_is_prefix_of_dstr(str,i,"ksh")||
 			check_if_str_is_prefix_of_dstr(str,i,"zsh")){i+=3;satisfy=1;}
 		else if(check_if_str_is_prefix_of_dstr(str,i,"sh")){i+=2;satisfy=1;}
-		if(satisfy&&str->data[i]=='\"')ret_code=4;//linux shell detected
+		if(satisfy&&str->data[i]=='\"')ret_code=4;//linux shell binary detected
 		goto restore_data_and_return;
 	}
 	//suffix like 32 and 64 is also allowed
@@ -65,22 +68,23 @@ int check_for_special_program_keyword(dstr*str,int left_index,int right_index)
 		check_if_str_is_prefix_of_dstr(str,i,"64"))i+=2;
 	//in case if there is wine or proton exe version (unlikely)
 	if(check_if_str_is_prefix_of_dstr(str,i,".exe"))i+=4;
-	if(str->data[i]=='\"')ret_code=2;//wine/proton detected
-	//restore the format
+	if(str->data[i]=='\"')ret_code=2;//wine or proton binary detected
+	//end of check time to restore the data to original
 	restore_data_and_return:;
 	str->data-=left_index;
+	str->length+=left_index;
 	str->data[left_index]=left_backup;
 	str->data[right_index]=right_backup;
 	str->data[right_index+1]=backup_char;
 	return ret_code;
 }
-dstr log_str;FILE *log_file;char *argv0;
-void append_char_to_dstr(dstr *str,char c,int escape_special)
+dstr log_str;FILE*log_file;char*argv0;
+void append_char_to_dstr(dstr*str,char c,int escape_special)
 {
 	if(str==&log_str&&log_file==NULL)
 	{
 		//because log file is an emergency it need to be prioritized! (have own safe code)
-		int i,j;char *log_file_path;for(i=-1;argv0[++i];);
+		int i,j;char*log_file_path;for(i=-1;argv0[++i];);
 		log_file_path=(char*)calloc(i+128,sizeof(char));
 		while(i--)if(argv0[i]=='\\'||argv0[i]=='/')break;
 		for(j=i+1,i=-1;++i<j;)log_file_path[i]=argv0[i];
@@ -117,7 +121,7 @@ void append_char_to_dstr(dstr *str,char c,int escape_special)
 	}
 	return;
 }
-void append_string_to_dstr(dstr*str,char *string,char end_symbol,int write_quotes)
+void append_string_to_dstr(dstr*str,char*string,char end_symbol,int write_quotes)
 {
 	if(write_quotes)append_char_to_dstr(str,'\"',0);
 	while(*string)append_char_to_dstr(str,*string++,write_quotes);
@@ -188,11 +192,11 @@ void convert_critical_argument_to_windows_format()
 	append_string_to_dstr(&log_str,"Done converting game argument!",'\n',0);
 	return;
 }
-dstr *argvx;char buffer[128];
+dstr*argvx;char buffer[128];
 int steam_index,argix,arglx,nested_shell_detected;
 void append_argument(char*value)
 {
-	dstr *str;int index,i,left=launch_command.length,right,end,write_arg_index=0;char c;
+	dstr*str;int index,i,left=launch_command.length,right,end,write_arg_index=0;char c;
 	sprintf(buffer,"Argument[%d]:",argix);
 	append_string_to_dstr(&log_str,buffer,' ',0);
 	if(arglx==argix)argvx=(dstr*)realloc(argvx,(arglx=arglx<<1|1)*sizeof(dstr));
@@ -290,7 +294,7 @@ void init_injector_path()
 	append_string_to_dstr(&log_str,"!",'\n',0);
 	return;
 }
-void create_full_file_injector_path(char *filename)
+void create_full_file_injector_path(char*filename)
 {
 	injector_path.length=injector_path_index;
 	while(*filename)append_char_to_dstr(&injector_path,*filename++,0);
@@ -298,9 +302,9 @@ void create_full_file_injector_path(char *filename)
 	append_char_to_dstr(&injector_path,'\0',0);
 	return;
 }
-FILE* open_file_on_injector_path(char *filename,char *mode)
+FILE* open_file_on_injector_path(char*filename,char*mode)
 {
-	FILE *f;
+	FILE*f;
 	create_full_file_injector_path(filename);
 	injector_path.data[injector_path.length-2]='\0';
 	f=fopen(injector_path.data+1,mode);
@@ -315,30 +319,39 @@ FILE* open_file_on_injector_path(char *filename,char *mode)
 	}
 	return f;
 }
-void check_an_injector(FILE *f,char *injector_name,dstr *arg)
+void check_an_injector(FILE*f,char*injector_name,dstr*arg)
 {
-	FILE *ff;int i;
+	FILE*ff;int i;char c;
 	ff=open_file_on_injector_path(injector_name,"r");
 	if(ff!=NULL)
 	{
 		fclose(ff);
 		fprintf(f,"start \"\" \"%s\"",injector_name);
-		for(i=-1;++i<arg->length;)fprintf(f," \"%%%c\"",arg->data[i]);
-		fprintf(f,"\n");
+		for(i=-1;++i<arg->length;)
+		{
+			if(arg->data[i]<'0'||'9'<arg->data[i])continue;
+			fprintf(f," \"%%");
+			for(--i;++i<arg->length&&'0'<=(c=arg->data[i])&&c<='9';)fputc(c,f);
+			fputc('\"',f);
+		}
+		fputc('\n',f);
 		append_string_to_dstr(&log_str,"found",' ',0);
 		append_string_to_dstr(&log_str,injector_name,' ',1);
-		append_string_to_dstr(&log_str,"on injector path!",'\n',0);
+		append_string_to_dstr(&log_str,"on injector path!",' ',0);
 	}
 	else
 	{
 		append_string_to_dstr(&log_str,injector_name,' ',1);
-		append_string_to_dstr(&log_str,"not found!",'\n',0);
+		append_string_to_dstr(&log_str,"not found!",' ',0);
 	}
+	append_string_to_dstr(&log_str,"Full list of argument index for that program: ",'[',0);
+	append_string_to_dstr(&log_str,arg->data,']',0);
+	append_char_to_dstr(&log_str,'\n',0);
 	return;
 }
-void load_injector_list(FILE *f,char*config_name)
+void load_injector_list(FILE*f,char*config_name)
 {
-	FILE*ff;int cur,max_arg;dstr arg,exe;
+	FILE*ff;int cur,max_arg,value;dstr arg,exe;
 	ff=open_file_on_injector_path(config_name,"r");
 	append_string_to_dstr(&log_str,"Opening config file:",' ',0);
 	append_string_to_dstr(&log_str,config_name,'\n',1);
@@ -349,14 +362,10 @@ void load_injector_list(FILE *f,char*config_name)
 		fprintf(ff,"#cmd inject version: %s\n",CMD_INJECT_VERSION);
 		fprintf(ff,"#note: you can list your custom injector here to be auto detected\n");
 		fprintf(ff,"#format: \"<injector exe>\" <arg_idx_a> <arg_idx_b> ... <arg_idx_n>\n");
-		fprintf(ff,"#arg_idx: is a number between \'1\' and \'9\' inclusive\n");
-		fprintf(ff,"# you could use \'0\' too as a valid arg_idx but\n");
-		fprintf(ff,"# it will be this program name (full path) and useless in most cases.\n");
-		fprintf(ff,"# for now you can add only up to 9 distinct arguments to the injector\n");
-		fprintf(ff,"# let me know if you want more arguments by posting issue on github\n");
-		fprintf(ff,"# if you have reasonable exuse I might add this support in the future\n");
-		fprintf(ff,"# but IMHO 9 distinct arguments is a lot already!\n");
-		fprintf(ff,"# you could also edit \"config.bat\" to whatever you like\n");
+		fprintf(ff,"#arg_idx: is a non negative integer but useful arguments start with 1\n");
+		fprintf(ff,"# if arg_idx is equal to zero (0) that contain this program name \n");
+		fprintf(ff,"# and it's fullpath which is useless for the injector arguments'\n");
+		fprintf(ff,"# remember you could also edit \"config.bat\" to whatever you like\n");
 		fprintf(ff,"# don't forget to remove the first commented line on \"config.bat\"!\n");
 		fprintf(ff,"# so that config.bat is not overwritten by this program\n");
 		fprintf(ff,"\"inject64.exe\" 1\n");
@@ -381,11 +390,19 @@ void load_injector_list(FILE *f,char*config_name)
 			{
 				max_arg=0;
 				while((cur=fgetc(ff))!=EOF&&(char)cur!='\r'&&(char)cur!='\n')
-					if('0'<=(char)cur&&(char)cur<='9')
+				{
+					if((char)cur<'0'||'9'<(char)cur)continue;
+					for(value=0;'0'<=(char)cur&&(char)cur<='9';cur=fgetc(ff))
 					{
+						value=10*value+(cur&15);
 						append_char_to_dstr(&arg,(char)cur,0);
-						if((cur&15)>max_arg)max_arg=cur&15;
 					}
+					if(value>max_arg)max_arg=value;
+					append_char_to_dstr(&arg,' ',0);
+					if(cur==EOF||(char)cur=='\r'||(char)cur=='\n')break;
+				}
+				if(!arg.length)*arg.data='\0';
+				else arg.data[arg.length-1]='\0';
 				if(max_arg<steam_index)check_an_injector(f,exe.data,&arg);
 				else
 				{
@@ -395,8 +412,10 @@ void load_injector_list(FILE *f,char*config_name)
 					append_string_to_dstr(&log_str,"program arguments needed ",'(',0);
 					sprintf(buffer,"%d) is exceeding number of given arguments ",max_arg);
 					append_string_to_dstr(&log_str,buffer,'(',0);
-					sprintf(buffer,"%d)!",steam_index-1);
-					append_string_to_dstr(&log_str,buffer,'\n',0);
+					sprintf(buffer,"%d)! here is the full prog argument: ",steam_index-1);
+					append_string_to_dstr(&log_str,buffer,'[',0);
+					append_string_to_dstr(&log_str,arg.data,']',0);
+					append_char_to_dstr(&log_str,'\n',0);
 				}
 			}
 		}
@@ -409,7 +428,7 @@ void load_injector_list(FILE *f,char*config_name)
 }
 FILE* generate_bat_config(char*bat_filename)
 {
-	FILE *f;
+	FILE*f;
 	f=open_file_on_injector_path(bat_filename,"w");
 	fprintf(f,"rem ove this line to make this config permanent! ");
 	fprintf(f,"cmd_inject version: %s\n",CMD_INJECT_VERSION);
@@ -423,7 +442,7 @@ FILE* generate_bat_config(char*bat_filename)
 int main(int argc,char**argv)
 {
 	//receiving argumnents parameter and initializing the program
-	int i,j,ret=-1;FILE *f,*ff;argv0=argv[0];
+	int i,j,ret=-1;FILE*f,*ff;argv0=argv[0];
 	sprintf(buffer,"cmd_inject version: %s",CMD_INJECT_VERSION);
 	append_string_to_dstr(&log_str,buffer,'\n',0);
 	append_string_to_dstr(&log_str,"\n===[Injector Argument]===",'\n',0);
@@ -454,11 +473,12 @@ int main(int argc,char**argv)
 		j=fgetc(f);
 		if('0'<=(char)j&&(char)j<='9')
 		{
-			i=fgetc(f);
-			if((char)i<=' '||(char)i=='\"')write_argument_to_file(ff,j&15);
-			else{fputc('%',ff);fputc(j,ff);}
+			for(i=j,j=0;'0'<=(char)i&&(char)i<='9';i=fgetc(f))j=10*j+(i&15);
+			if((char)i<=' '||(char)i=='\"')write_argument_to_file(ff,j);
+			else fprintf(ff,"%%%d",j);
 		}
 		else{fputc('%',ff);i=j;}
+		if(i==EOF)break;
 	}
 	fclose(f);
 	if(launch_command.data!=NULL)launch_command.data[--launch_command.length]='\0';
@@ -485,7 +505,7 @@ int main(int argc,char**argv)
 	}
 	append_string_to_dstr(&log_str,"\n===[Launch Command]===",'\n',0);
 	append_string_to_dstr(&log_str,launch_command.data,'\0',0);
-	//cleaning up, freeing memory, and launching injected launch command
+	//cleaning up and freeing memory
 	if(argvx)
 	{
 		for(i=-1;++i<argix;)if(argvx[i].data)free(argvx[i].data);
@@ -493,7 +513,7 @@ int main(int argc,char**argv)
 	}
 	if(injector_path.data)free(injector_path.data);
 	if(log_str.data)free(log_str.data);
-	ret=system(launch_command.data);
+	ret=system(launch_command.data);//launching injected launch command
 	free(launch_command.data);
 	return ret;
 }
